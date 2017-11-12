c1d22474bad03c4fb7d0abe66e57f662465d496f
dareczeg <32062687+dareczeg@users.noreply.github.com>
2017-11-13 00:22:58 +0100

    Add files via upload

---
+++ Równianiekwadratowe/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Równianiekwadratowe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Klasa Delta = new Klasa();
+
+            Delta.UstawDlugosc(Convert.ToDouble(textBox1.Text));
+            Delta.UstawSzerokosc(Convert.ToDouble(textBox2.Text));
+            Delta.UstawWysokosc(Convert.ToDouble(textBox3.Text));
+            double obj1 = Delta.calculateDelta();
+            label6.Text = Convert.ToString(obj1);
+            double obj2 = Delta.calculateX1();
+            label4.Text = Convert.ToString(obj2);
+            double obj3 = Delta.calculateX2();
+            label5.Text = Convert.ToString(obj3);
+            
+            
+
+        }
+    }
+}
---
+++ Równianiekwadratowe/Klasa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Równianiekwadratowe
+{
+    class Klasa
+    {
+        double aa, be, ce, delta, x1, x2, wynik1, wynik2;
+        public void UstawDlugosc(double a)
+        {
+            aa = a;
+        }
+        public void UstawSzerokosc(double b)
+        {
+            be = b;
+        }
+        public void UstawWysokosc(double c)
+        {
+            ce = c;
+        }
+        public double calculateDelta() {
+            delta = Math.Pow(be, 2) - 4 * aa * ce;
+            return delta;
+        }
+        public double calculateX1() {
+            x1 = -be + Math.Sqrt(delta);
+            wynik1 = x1 / 2;
+            return wynik1;
+        }
+        public double calculateX2()
+        {
+            x2 = -be - Math.Sqrt(delta);
+            wynik2 = x2 / 2;
+            return wynik2;
+        }
+
+    }
+}
